55d324888f9baa2fbc3068cbd46afb91dbf865f6
xilele777 <2519175810@qq.com>
2026-03-18 00:45:15 +0800

    260318优化

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
 Write-Host "Building for production..."
+if (Test-Path -LiteralPath "public") {
+  Remove-Item -LiteralPath "public" -Recurse -Force
+}
 hugo --minify
 Write-Host "Build complete. content is in 'public' directory."
